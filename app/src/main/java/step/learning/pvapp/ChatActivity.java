baebdec0cfb7a44a4b85eba1fedccb3734e08022
Bodu_wk69 <Bodu_wk69@student.itstep.org>
2024-04-19 19:51:51 +0300

    ChatUpdate

--- app/src/main/java/step/learning/pvapp/ChatActivity.java
+++ app/src/main/java/step/learning/pvapp/ChatActivity.java
@@ -1,13 +1,22 @@
 package step.learning.pvapp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -16,6 +25,7 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -55,14 +65,20 @@ public class ChatActivity extends AppCompatActivity {
     private final Handler handler = new Handler();
     private MediaPlayer incomingMessage;
     private String author = null;
+    private Animation chatVoiceAnimation;
+    private ImageView chat_voice_btn;
+    private boolean isVoiceOn = true;
 
 
-
-    private void timer(){
-        try{pool.submit(this::loadChatMessages);}
-        catch (Exception ignored) {}
+    private void timer() {
+        try {
+            pool.submit(this::loadChatMessages);
+        } catch (Exception ignored) {
+        }
         handler.postDelayed(this::timer, 3000);
     }
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,10 +95,60 @@ public class ChatActivity extends AppCompatActivity {
         findViewById(R.id.chat_btn_send).setOnClickListener(this::sendMessageClick);
         etAuthor = findViewById(R.id.chat_et_name);
         etMessage = findViewById(R.id.chat_et_message);
-        bgOwn = AppCompatResources.getDrawable(getApplicationContext(),R.drawable.chat_bg_own);
-        bgOther = AppCompatResources.getDrawable(getApplicationContext(),R.drawable.chat_bg_other);
+        bgOwn = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.chat_bg_own);
+        bgOther = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.chat_bg_other);
         messagesScroller = findViewById(R.id.chat_messages_scroller);
-        incomingMessage = MediaPlayer.create(this,R.raw.income);
+        incomingMessage = MediaPlayer.create(this, R.raw.income);
+        chat_voice_btn = findViewById(R.id.chat_voice_btn);
+        chat_voice_btn.setOnClickListener(this::onChatVoiceClick);
+        chatVoiceAnimation = AnimationUtils.loadAnimation(this, R.anim.chat_voice_btn_anim);
+        messagesScroller.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                return v.performClick();
+            } else {
+                hideSoftwareKeyboard();
+            }
+            return true;
+        });
+        tryRestoreAuthor();
+    }
+
+    private void hideSoftwareKeyboard() {
+        View focusedView = getCurrentFocus();
+        if (focusedView != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+        }
+    }
+
+    private void onChatVoiceClick(View view) {
+        view.startAnimation(chatVoiceAnimation);
+        isVoiceOn = !isVoiceOn;
+        ImageView imageView = (ImageView) view;
+        if (!isVoiceOn) {
+            imageView.setColorFilter(ContextCompat.getColor(this, R.color.ttt_X_score_color), android.graphics.PorterDuff.Mode.SRC_IN);
+        } else {
+            imageView.setColorFilter(ContextCompat.getColor(this, R.color.calc_primary_color), android.graphics.PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    private void tryRestoreAuthor() {
+        try (SQLiteDatabase db = openOrCreateDatabase("cnt_db", MODE_PRIVATE, null)) {
+            db.execSQL("CREATE TABLE IF NOT EXISTS auth_stat(id ROWID,dtt DATETIME, author VARCHAR(64))");
+            Cursor resultSet = db.rawQuery("SELECT author FROM auth_stat ORDER BY dtt DESC", null);
+            if (resultSet.moveToFirst()){
+                etAuthor.setText(resultSet.getString(0));
+            }
+            resultSet.close();
+        }
+    }
+
+    private void saveLastAuthor(){
+        try(SQLiteDatabase db = openOrCreateDatabase ("cnt_db", MODE_PRIVATE,  null) ){
+            db.execSQL(
+                     "INSERT INTO auth_stat(author, dtt) VALUES(?, CURRENT_TIMESTAMP)",
+                    new String[]{etAuthor.getText().toString()});
+        }
     }
 
     private void loadChatMessages() {
@@ -113,22 +179,26 @@ public class ChatActivity extends AppCompatActivity {
 
     private void updateMessagesView() {
         String author = etAuthor.getText().toString();
-        boolean isFirst =  messagesContainer.getChildCount() == 0;
+        boolean isFirst = messagesContainer.getChildCount() == 0;
         boolean needSound = false;
-        for( ChatMessage message : chatMessages ) {
-            if( message.getTag() == null ) {
+        for (ChatMessage message : chatMessages) {
+            if (message.getTag() == null) {
                 boolean isOwn = author.equals(message.getAuthor());
-                View view = messageView(message,isOwn);
+                View view = messageView(message, isOwn);
                 needSound |= !isOwn;
                 messagesContainer.addView(view);
                 message.setTag(view);
-                if(needSound && !isFirst) incomingMessage.start();
+                if (needSound && !isFirst && isVoiceOn) {
+                    incomingMessage.start();
+                    chat_voice_btn.startAnimation(chatVoiceAnimation);
+                }
             }
         }
-    //
+        //
         messagesScroller.post(() -> messagesScroller.fullScroll(View.FOCUS_DOWN));
     }
-    private View messageView(ChatMessage message,boolean isOwn){
+
+    private View messageView(ChatMessage message, boolean isOwn) {
         LinearLayout linearLayout = new LinearLayout(this);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -137,14 +207,13 @@ public class ChatActivity extends AppCompatActivity {
         linearLayout.setLayoutParams(layoutParams);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         layoutParams.gravity = isOwn ? Gravity.END : Gravity.START;
-        layoutParams.setMargins(10,15,10,15);
-        linearLayout.setBackground(isOwn? bgOwn : bgOther);
-        linearLayout.setPadding(10,15,10,15);
-
+        layoutParams.setMargins(10, 15, 10, 15);
+        linearLayout.setBackground(isOwn ? bgOwn : bgOther);
+        linearLayout.setPadding(10, 15, 10, 15);
 
 
         TextView textView = new TextView(this);
-        textView.setText(getString(R.string.chat_message_line1,dateTimeFormat.format(message.getMoment()),message.getAuthor()));
+        textView.setText(getString(R.string.chat_message_line1, dateTimeFormat.format(message.getMoment()), message.getAuthor()));
         linearLayout.addView(textView);
         textView = new TextView(this);
         textView.setText(message.getText());
@@ -187,27 +256,27 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void sendMessageClick(View view) {
-        if(author == null) {
+        if (author == null) {
             author = etAuthor.getText().toString();
-            if(author.isEmpty()) {
+            if (author.isEmpty()) {
                 Toast.makeText(this, R.string.chat_no_author_alert, Toast.LENGTH_SHORT).show();
                 author = null;
                 return;
-            }
-            else {
+            } else {
+                saveLastAuthor();
                 etAuthor.setEnabled(false);
             }
         }
         String msg = etMessage.getText().toString();
-        if(msg.isEmpty()) {
+        if (msg.isEmpty()) {
             Toast.makeText(this, R.string.chat_no_message_alert, Toast.LENGTH_SHORT).show();
             return;
         }
         ChatMessage message = new ChatMessage();
-        message.setAuthor( author );
-        message.setText( msg );
+        message.setAuthor(author);
+        message.setText(msg);
 
-        new Thread( () -> sendChatMessage(message) ).start();
+        new Thread(() -> sendChatMessage(message)).start();
 
     }
 
